48892d5df09bef2ab05e4094bfb1261ded4cb24e
coderTonyChan <101992315@qq.com>
2018-01-18 16:53:17 +0800

    登录的问题 腾讯不允许在小程序里请求

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,18 +1,41 @@
 /**
  * 用户code 换取 session_key
  * @type {String}
+ *就2个属性
+obj.openid = 'oeuj50KHMqsh5kYZYWQJuwmY5yG0'
+obj.expires_in = '7200'
  */
 export const USER_SPECICAL_INFO = 'userSpecialInfo'
 
 /**
  * 用户信息
  * @type {String}
+ *  avatarUrl:"https://wx.qlogo.cn/mmopen/vi_32/ca6wwg1Wp6YMngsyZqEGCxeUY5Wz91BYzIBiczapXt9yhNrlZRibM4TfhIEpU3yCoBoicyvpB3BFRbuXaeAXO0p9w/0"
+    city:"Dongguan"
+    country:"China"
+    gender:1
+    language:"zh_CN"
+    nickName:"鱼初见"
+    province:"Guangdong"}
  */
 export const USER_INFO = 'userInfo'
 
 /**
  * 系统信息
  * @type {String}
+ *  SDKVersion:"1.9.0"
+    brand:"devtools"
+    fontSizeSetting:16
+    language:"zh_CN"
+    model:"iPhone 7"
+    pixelRatio:2
+    platform:"devtools"
+    screenHeight:667
+    screenWidth:375
+    system:"iOS 10.0.1"
+    version:"6.5.6"
+    windowHeight:555
+    windowWidth:375
  */
 export const SYSTEM_INFO = 'systemInfo'
 
